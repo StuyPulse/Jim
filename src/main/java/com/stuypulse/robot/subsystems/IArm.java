821fee58c2f1ce5bbd858d09ff383d5395796eeb
anivanchen <ivanchen07@gmail.com>
2023-01-25 14:58:46 -0500

    Add IArm
    Co-authored-by: independence106 <independence106@users.noreply.github.com>

---
+++ src/main/java/com/stuypulse/robot/subsystems/IArm.java
@@ -0,0 +1,31 @@
+package com.stuypulse.robot.subsystems;
+
+import com.stuypulse.robot.subsystems.arm.Arm;
+import com.stuypulse.robot.subsystems.arm.SimArm;
+import com.stuypulse.stuylib.math.Angle;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Tasks:
+ * - Get Angle from arm and wrist joint
+ * - Control arm and wrist joints
+ * - Set target angle
+ * - Go to target angle
+ */
+public abstract class IArm extends SubsystemBase {
+
+    public static IArm getInstance() {
+        return RobotBase.isReal() ? new Arm() : new SimArm();
+    }
+    
+    public abstract Angle getShoulderAngle();
+    public abstract Angle getWristAngle();
+
+    public abstract void setTargetShoulderAngle(Angle angle);
+    public abstract void setTargetWristAngle(Angle angle);
+
+    public abstract boolean isShoulderAtAngle(double maxError);
+    public abstract boolean isWristAtAngle(double maxError);
+}
