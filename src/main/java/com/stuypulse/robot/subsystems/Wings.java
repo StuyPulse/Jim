eab9e0ee361f6e65b3896a8ae09474b2e53a3df7
Myles Pasetsky <mp3blue99@gmail.com>
2023-01-26 13:08:44 -0500

    Push unfixed version of the wing code

--- src/main/java/com/stuypulse/robot/subsystems/Wings.java
+++ src/main/java/com/stuypulse/robot/subsystems/Wings.java
@@ -4,70 +4,73 @@ import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.stuylib.util.StopWatch;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Wings extends SubsystemBase {
+public class Wings extends IWings {
 
     private final StopWatch timer;
 
-    private double currentTime1;
-    private double currentTime2;
+    private double leftDeployTime;
+    private double rightDeployTime;
 
     // left solenoids
-    private final Solenoid deploy1;
-    private final Solenoid pancake1;
+    private final DoubleSolenoid leftDeploy;
+    private final Solenoid leftPancake;
 
     // right solenoids
-    private final Solenoid deploy2;
-    private final Solenoid pancake2;
+    private final DoubleSolenoid rightDeploy;
+    private final Solenoid rightPancake;
 
     public Wings() {
-        deploy1 = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.DEPLOY_1);
-        deploy2 = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.DEPLOY_2);
-        pancake1 = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.PANCAKE_1);
-        pancake2 = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.PANCAKE_2);
         timer = new StopWatch();
-        currentTime1 = -1.0;
-        currentTime2 = -1.0;
+
+        leftDeploy = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.LEFT_DEPLOY_FORWARD, Ports.Wings.LEFT_DEPLOY_REVERSE);
+        leftPancake = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.LEFT_PANCAKE);
+        leftDeployTime = -1.0;
+
+        rightDeploy = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.RIGHT_DEPLOY_FORWARD, Ports.Wings.RIGHT_DEPLOY_REVERSE);
+        rightPancake = new Solenoid(PneumaticsModuleType.CTREPCM, Ports.Wings.LEFT_PANCAKE);
+        rightDeployTime = -1.0;
     }
 
-    public void extendWing1() {
-        if (pancake1.get() && !deploy1.get()) {
-            pancake1.set(false);
-            currentTime1 = timer.getTime();
+    public void extendLeft() {
+        if (leftPancake.get() && !leftDeploy.get()) {
+            leftPancake.set(false);
+            leftDeployTime = timer.getTime();
         }
     }
 
-    public void retractWing1() {
-        if (!pancake1.get() && deploy1.get()) {
-            deploy1.set(false);
+    public void retractLeft() {
+        if (!leftPancake.get() && leftDeploy.get()) {
+            leftDeploy.set(false);
         }
     }
 
-    public void extendWing2() {
-        if(pancake2.get() && !deploy2.get()){
-            pancake2.set(false);
-            currentTime2 = timer.getTime();
+    public void extendRight() {
+        if(rightPancake.get() && !rightDeploy.get()){
+            rightPancake.set(false);
+            rightDeployTime = timer.getTime();
         }
     }
 
-    public void retractWing2() {
-        if(!pancake2.get() && deploy2.get()){
-            deploy2.set(false);
+    public void retractRight() {
+        if(!rightPancake.get() && rightDeploy.get()){
+            rightDeploy.set(false);
         }
     }
 
     @Override
     public void periodic() {
-        if(currentTime1 > 0 && timer.getTime() - currentTime1 >= Settings.Wings.PANCAKE_DELAY){
-            deploy1.set(true);
-            currentTime1 = -1.0;
+        if(leftDeployTime > 0 && timer.getTime() - leftDeployTime >= Settings.Wings.PANCAKE_DELAY){
+            leftDeploy.set(true);
+            leftDeployTime = -1.0;
         }
-        if(currentTime1 > 0 && timer.getTime() - currentTime2 >= Settings.Wings.PANCAKE_DELAY){
-            deploy2.set(true);
-            currentTime2 = -1.0;
+        if(leftDeployTime > 0 && timer.getTime() - rightDeployTime >= Settings.Wings.PANCAKE_DELAY){
+            rightDeploy.set(true);
+            rightDeployTime = -1.0;
         }
     }
 }
