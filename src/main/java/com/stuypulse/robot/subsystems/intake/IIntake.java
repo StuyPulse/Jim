db2f88a318b1e2982eca0e316bee53de326f6be9
Eric Lin <ericlin071906@gmail.com>
2023-01-28 10:30:58 -0500

    remove comment in IIntake

--- src/main/java/com/stuypulse/robot/subsystems/intake/IIntake.java
+++ src/main/java/com/stuypulse/robot/subsystems/intake/IIntake.java
@@ -1,31 +1,5 @@
 package com.stuypulse.robot.subsystems.intake;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-/*
- * fields: 
- * - 2 motors
- * 
- * methods:
- * - cube intake
- * - cone intake
- * - cube outtake
- * - cone outtake
- * - stalling
- * - getangle (of arm) to know if intake is flipped
- * 
- * ports
- * - 2 motors
- * - IR sensor
- * 
- * Contants
- * - Stalling Constant
- * 
- * periodic:
- * - get angle (smartnumber)
- * - isStalling (smartnumber)
- * - is IR sensor tripped smartnumber
- */
 
 public abstract class IIntake extends SubsystemBase {
     private static IIntake instance;
