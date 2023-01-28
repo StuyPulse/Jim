14eba9b624960721a2f9f2ad276caac3039e14b1
anivanchen <ivanchen07@gmail.com>
2023-01-28 14:52:09 -0500

    Add Trajectories without real values

--- src/main/java/com/stuypulse/robot/constants/ArmTrajectories.java
+++ src/main/java/com/stuypulse/robot/constants/ArmTrajectories.java
@@ -5,8 +5,93 @@ import com.stuypulse.robot.util.ArmTrajectory;
 public interface ArmTrajectories {
     ArmTrajectory NEUTRAL = new ArmTrajectory().addState(-90, 90);
 
-    public interface High {
-        ArmTrajectory CONE_READY = new ArmTrajectory().addState(0, 0);
-        ArmTrajectory CUBE_READY = new ArmTrajectory().addState(0, 0);
+    public interface SameSide {
+        public interface High {
+            public interface Cone {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+            public interface Cube {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+        }
+        public interface Mid {
+            public interface Cone {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+            public interface Cube {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+        }
+        public interface Low {
+            public interface Cone {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+            public interface Cube {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+        }
+    }
+
+    public interface OppositeSide {
+        public interface High {
+            public interface Cone {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+            public interface Cube {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+        }
+        public interface Mid {
+            public interface Cone {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+            public interface Cube {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+        }
+        public interface Low {
+            public interface Cone {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+            public interface Cube {
+                ArmTrajectory READY = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory INTAKE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+                ArmTrajectory SCORE_TO_NEUTRAL = new ArmTrajectory().addState(0, 0);
+            }
+        }
     }
 }
