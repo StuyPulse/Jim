970c900eb1bbb442955c111106f0fdcc240870d2
Oblivion409 <ben@goldfisher.com>
2023-01-28 11:31:01 -0500

    Add field objects to swerve and odometry

--- src/main/java/com/stuypulse/robot/subsystems/odometry/Odometry.java
+++ src/main/java/com/stuypulse/robot/subsystems/odometry/Odometry.java
@@ -9,7 +9,6 @@ import com.stuypulse.robot.subsystems.vision.IVision.Result;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
@@ -24,6 +23,7 @@ public class Odometry extends IOdometry {
         poseEstimator = new SwerveDrivePoseEstimator(swerve.getKinematics(), swerve.getGyroAngle(), swerve.getModulePositions(), new Pose2d());
         poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(0.01, 0.1, Units.degreesToRadians(3)));
         field = new Field2d();
+        swerve.initFieldObjects(field);
     }
 
     public Pose2d getPose() {
