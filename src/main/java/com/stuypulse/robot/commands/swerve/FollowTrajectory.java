f0fddb7d5afc6daa4b8b3f03749c45e9cb577b07
Oblivion409 <ben@goldfisher.com>
2023-01-28 15:21:53 -0500

    Add multi path to map function in FollowTrajectory
    Co-authored-by: ShauryaSen <ShauryaSen@users.noreply.github.com>

--- src/main/java/com/stuypulse/robot/commands/swerve/FollowTrajectory.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/FollowTrajectory.java
@@ -1,16 +1,15 @@
 package com.stuypulse.robot.commands.swerve;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPlannerTrajectory.PathPlannerState;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
-import com.stuypulse.robot.constants.Motors.Swerve;
 import com.stuypulse.robot.constants.Settings.Swerve.Motion;
 import com.stuypulse.robot.subsystems.odometry.IOdometry;
-import com.stuypulse.robot.subsystems.odometry.Odometry;
 import com.stuypulse.robot.subsystems.swerve.SwerveDrive;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -19,6 +18,19 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class FollowTrajectory extends PPSwerveControllerCommand {
+
+	public static HashMap<String, PathPlannerTrajectory> getSeparatedPaths(List<PathPlannerTrajectory> paths, String... names) {
+		if (paths.size() != names.length)
+			throw new IllegalArgumentException("Invalid number of path names given to FollowTrajectory.getSeparatedPaths");
+
+		HashMap<String, PathPlannerTrajectory> map = new HashMap<String, PathPlannerTrajectory>();
+
+		for (int i = 0; i < names.length; i++) {
+			map.put(names[i], paths.get(i));
+		}
+
+		return map;
+	}
 
 	private boolean robotRelative;
 	private PathPlannerTrajectory path;
