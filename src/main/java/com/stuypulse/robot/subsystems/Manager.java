9fa710056fca15cfecefac30120ea2a91c0b68fe
Ivan Chen <ivanchen07@gmail.com>
2023-01-28 19:31:36 -0500

    it's a boy

--- src/main/java/com/stuypulse/robot/subsystems/Manager.java
+++ src/main/java/com/stuypulse/robot/subsystems/Manager.java
@@ -80,105 +80,105 @@ public class Manager {
                         switch (currentPiece) {
                             case CONE:
                                 switch (currentMode) {
-                                    case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case INTAKE: 
+                                    case READY: return SameSide.High.Cone.READY;
+                                    case SCORE: return SameSide.High.Cone.SCORE;
+                                    case NEUTRAL: return SameSide.High.Cone.SCORE_TO_NEUTRAL;
                                 }
                             case CUBE: 
                                 switch (currentMode) {
                                     case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case READY: return SameSide.High.Cube.READY;
+                                    case SCORE: return SameSide.High.Cube.SCORE;
+                                    case NEUTRAL: return SameSide.High.Cube.SCORE_TO_NEUTRAL;
                                 }
                         }
                         case MID:
                         switch (currentPiece) {
                             case CONE:
                                 switch (currentMode) {
-                                    case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case INTAKE: 
+                                    case READY: return SameSide.Mid.Cone.READY;
+                                    case SCORE: return SameSide.Mid.Cone.SCORE;
+                                    case NEUTRAL: return SameSide.Mid.Cone.SCORE_TO_NEUTRAL;
                                 }
                             case CUBE: 
                                 switch (currentMode) {
                                     case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case READY: return SameSide.Mid.Cube.READY;
+                                    case SCORE: return SameSide.Mid.Cube.SCORE;
+                                    case NEUTRAL: return SameSide.Mid.Cube.SCORE_TO_NEUTRAL;
                                 }
                         }
                         case LOW:
                         switch (currentPiece) {
                             case CONE:
                                 switch (currentMode) {
-                                    case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case INTAKE: 
+                                    case READY: return SameSide.Low.Cone.READY;
+                                    case SCORE: return SameSide.Low.Cone.SCORE;
+                                    case NEUTRAL: return SameSide.Low.Cone.SCORE_TO_NEUTRAL;
                                 }
                             case CUBE: 
                                 switch (currentMode) {
                                     case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case READY: return SameSide.Low.Cube.READY;
+                                    case SCORE: return SameSide.Low.Cube.SCORE;
+                                    case NEUTRAL: return SameSide.Low.Cube.SCORE_TO_NEUTRAL;
                                 }
                         }
                 }
-            case OPPOSITE:
+                case OPPOSITE:
                 switch (currentLevel) {
                     case HIGH:
                         switch (currentPiece) {
                             case CONE:
                                 switch (currentMode) {
-                                    case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case INTAKE: 
+                                    case READY: return OppositeSide.High.Cone.READY;
+                                    case SCORE: return OppositeSide.High.Cone.SCORE;
+                                    case NEUTRAL: return OppositeSide.High.Cone.SCORE_TO_NEUTRAL;
                                 }
                             case CUBE: 
                                 switch (currentMode) {
                                     case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case READY: return OppositeSide.High.Cube.READY;
+                                    case SCORE: return OppositeSide.High.Cube.SCORE;
+                                    case NEUTRAL: return OppositeSide.High.Cube.SCORE_TO_NEUTRAL;
                                 }
                         }
                         case MID:
                         switch (currentPiece) {
                             case CONE:
                                 switch (currentMode) {
-                                    case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case INTAKE: 
+                                    case READY: return OppositeSide.Mid.Cone.READY;
+                                    case SCORE: return OppositeSide.Mid.Cone.SCORE;
+                                    case NEUTRAL: return OppositeSide.Mid.Cone.SCORE_TO_NEUTRAL;
                                 }
                             case CUBE: 
                                 switch (currentMode) {
                                     case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case READY: return OppositeSide.Mid.Cube.READY;
+                                    case SCORE: return OppositeSide.Mid.Cube.SCORE;
+                                    case NEUTRAL: return OppositeSide.Mid.Cube.SCORE_TO_NEUTRAL;
                                 }
                         }
                         case LOW:
                         switch (currentPiece) {
                             case CONE:
                                 switch (currentMode) {
-                                    case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case INTAKE: 
+                                    case READY: return OppositeSide.Low.Cone.READY;
+                                    case SCORE: return OppositeSide.Low.Cone.SCORE;
+                                    case NEUTRAL: return OppositeSide.Low.Cone.SCORE_TO_NEUTRAL;
                                 }
                             case CUBE: 
                                 switch (currentMode) {
                                     case INTAKE:
-                                    case READY:
-                                    case SCORE:
-                                    case NEUTRAL:
+                                    case READY: return OppositeSide.Low.Cube.READY;
+                                    case SCORE: return OppositeSide.Low.Cube.SCORE;
+                                    case NEUTRAL: return OppositeSide.Low.Cube.SCORE_TO_NEUTRAL;
                                 }
                         }
                 }
