d291c94bcb34e25bdc7c150809b7668ba8a73c86
Ivan Chen <ivanchen07@gmail.com>
2023-01-29 20:46:50 -0500

    Separated switch into methods

--- src/main/java/com/stuypulse/robot/subsystems/Manager.java
+++ src/main/java/com/stuypulse/robot/subsystems/Manager.java
@@ -2,7 +2,7 @@ package com.stuypulse.robot.subsystems;
 
 import static com.stuypulse.robot.constants.ArmTrajectories.*;
 
-import com.stuypulse.robot.constants.ArmTrajectories;
+import com.stuypulse.robot.constants.ArmTrajectories.SameSide.*;
 import com.stuypulse.robot.util.ArmTrajectory;
 
 public class Manager {
@@ -39,32 +39,32 @@ public class Manager {
         NEUTRAL
     }
 
-    private Piece currentPiece;
-    private Level currentLevel;
-    private Side currentSide;
-    private Mode currentMode;
+    private Piece piece;
+    private Level level;
+    private Side side;
+    private Mode mode;
 
     public Manager() {
-        this.currentPiece = Piece.CONE;
-        this.currentLevel = Level.HIGH;
-        this.currentSide = Side.SAME;
-        this.currentMode = Mode.NEUTRAL;
+        this.piece = Piece.CONE;
+        this.level = Level.HIGH;
+        this.side = Side.SAME;
+        this.mode = Mode.NEUTRAL;
     }
 
     public Level getLevel() {
-        return currentLevel;
+        return level;
     }
 
     public Piece getPiece() {
-        return currentPiece;
+        return piece;
     }
 
     public Side getSide() {
-        return currentSide;
+        return side;
     }
 
     public Mode getMode() {
-        return currentMode;
+        return mode;
     }
 
     public ArmTrajectory append(ArmTrajectory path, ArmTrajectory pathToAppend) {
@@ -79,137 +79,112 @@ public class Manager {
         return path.switchSides();
     }
 
-    public ArmTrajectory getPath(Side side, Mode mode) {
-        currentSide = side;
-        currentMode = mode;
+    public ArmTrajectory switchConeHigh() {
+        System.out.println("hello");
+        switch (mode) {
+            case INTAKE: 
+            case READY: return High.Cone.READY;
+            case SCORE: return High.Cone.SCORE;
+            case NEUTRAL: return High.Cone.SCORE_TO_NEUTRAL;
+        }
+        return NEUTRAL;
+    }
+    public ArmTrajectory switchConeMid() {
+        switch (mode) {
+            case INTAKE: 
+            case READY: return Mid.Cone.READY;
+            case SCORE: return Mid.Cone.SCORE;
+            case NEUTRAL: return Mid.Cone.SCORE_TO_NEUTRAL;
+        }
+        return NEUTRAL;
+    }
+    public ArmTrajectory switchConeLow() {
+        switch (mode) {
+            case INTAKE: 
+            case READY: return Low.Cone.READY;
+            case SCORE: return Low.Cone.SCORE;
+            case NEUTRAL: return Low.Cone.SCORE_TO_NEUTRAL;
+        }
+        return NEUTRAL;
+    }
 
-        switch (currentSide) {
+    public ArmTrajectory switchCubeHigh() {
+        switch (mode) {
+            case INTAKE:
+            case READY: return High.Cube.READY;
+            case SCORE: return High.Cube.SCORE;
+            case NEUTRAL: return High.Cube.SCORE_TO_NEUTRAL;
+        }
+        return NEUTRAL;
+    }
+    public ArmTrajectory switchCubeMid() {
+        switch (mode) {
+            case INTAKE:
+            case READY: return Mid.Cube.READY;
+            case SCORE: return Mid.Cube.SCORE;
+            case NEUTRAL: return Mid.Cube.SCORE_TO_NEUTRAL;
+        }
+        return NEUTRAL;
+    }
+    public ArmTrajectory switchCubeLow() {
+        switch (mode) {
+            case INTAKE:
+            case READY: return Low.Cube.READY;
+            case SCORE: return Low.Cube.SCORE;
+            case NEUTRAL: return Low.Cube.SCORE_TO_NEUTRAL;
+        }
+        return NEUTRAL;
+    }
+
+    public ArmTrajectory switchCone() {
+        switch (level) {
+            case HIGH: return switchConeHigh();
+            case MID: return switchConeMid();
+            case LOW: return switchConeLow();
+            default: return NEUTRAL;
+        }
+    }
+    public ArmTrajectory switchCube() {
+        switch (level) {
+            case HIGH: return switchCubeHigh();
+            case MID: return switchCubeMid();
+            case LOW: return switchCubeLow();
+            default: return NEUTRAL;
+        }
+    }
+
+    public ArmTrajectory getTrajectory(Side side, Mode mode) {
+        this.side = side;
+        this.mode = mode;
+
+        switch (side) {
             case SAME:
-                switch (currentLevel) {
-                    case HIGH:
-                        switch (currentPiece) {
-                            case CONE:
-                                switch (currentMode) {
-                                    case INTAKE: 
-                                    case READY: return SameSide.High.Cone.READY;
-                                    case SCORE: return SameSide.High.Cone.SCORE;
-                                    case NEUTRAL: return SameSide.High.Cone.SCORE_TO_NEUTRAL;
-                                }
-                            case CUBE: 
-                                switch (currentMode) {
-                                    case INTAKE:
-                                    case READY: return SameSide.High.Cube.READY;
-                                    case SCORE: return SameSide.High.Cube.SCORE;
-                                    case NEUTRAL: return SameSide.High.Cube.SCORE_TO_NEUTRAL;
-                                }
-                        }
-                        case MID:
-                        switch (currentPiece) {
-                            case CONE:
-                                switch (currentMode) {
-                                    case INTAKE: 
-                                    case READY: return SameSide.Mid.Cone.READY;
-                                    case SCORE: return SameSide.Mid.Cone.SCORE;
-                                    case NEUTRAL: return SameSide.Mid.Cone.SCORE_TO_NEUTRAL;
-                                }
-                            case CUBE: 
-                                switch (currentMode) {
-                                    case INTAKE:
-                                    case READY: return SameSide.Mid.Cube.READY;
-                                    case SCORE: return SameSide.Mid.Cube.SCORE;
-                                    case NEUTRAL: return SameSide.Mid.Cube.SCORE_TO_NEUTRAL;
-                                }
-                        }
-                        case LOW:
-                        switch (currentPiece) {
-                            case CONE:
-                                switch (currentMode) {
-                                    case INTAKE: 
-                                    case READY: return SameSide.Low.Cone.READY;
-                                    case SCORE: return SameSide.Low.Cone.SCORE;
-                                    case NEUTRAL: return SameSide.Low.Cone.SCORE_TO_NEUTRAL;
-                                }
-                            case CUBE: 
-                                switch (currentMode) {
-                                    case INTAKE:
-                                    case READY: return SameSide.Low.Cube.READY;
-                                    case SCORE: return SameSide.Low.Cube.SCORE;
-                                    case NEUTRAL: return SameSide.Low.Cube.SCORE_TO_NEUTRAL;
-                                }
-                        }
+                switch (piece) {
+                    case CONE: return switchCone();
+                    case CUBE: return switchCube();
                 }
-                case OPPOSITE:
-                switch (currentLevel) {
-                    case HIGH:
-                        switch (currentPiece) {
-                            case CONE:
-                                switch (currentMode) {
-                                    case INTAKE: 
-                                    case READY: return OppositeSide.High.Cone.READY;
-                                    case SCORE: return OppositeSide.High.Cone.SCORE;
-                                    case NEUTRAL: return OppositeSide.High.Cone.SCORE_TO_NEUTRAL;
-                                }
-                            case CUBE: 
-                                switch (currentMode) {
-                                    case INTAKE:
-                                    case READY: return OppositeSide.High.Cube.READY;
-                                    case SCORE: return OppositeSide.High.Cube.SCORE;
-                                    case NEUTRAL: return OppositeSide.High.Cube.SCORE_TO_NEUTRAL;
-                                }
-                        }
-                        case MID:
-                        switch (currentPiece) {
-                            case CONE:
-                                switch (currentMode) {
-                                    case INTAKE: 
-                                    case READY: return OppositeSide.Mid.Cone.READY;
-                                    case SCORE: return OppositeSide.Mid.Cone.SCORE;
-                                    case NEUTRAL: return OppositeSide.Mid.Cone.SCORE_TO_NEUTRAL;
-                                }
-                            case CUBE: 
-                                switch (currentMode) {
-                                    case INTAKE:
-                                    case READY: return OppositeSide.Mid.Cube.READY;
-                                    case SCORE: return OppositeSide.Mid.Cube.SCORE;
-                                    case NEUTRAL: return OppositeSide.Mid.Cube.SCORE_TO_NEUTRAL;
-                                }
-                        }
-                        case LOW:
-                        switch (currentPiece) {
-                            case CONE:
-                                switch (currentMode) {
-                                    case INTAKE: 
-                                    case READY: return OppositeSide.Low.Cone.READY;
-                                    case SCORE: return OppositeSide.Low.Cone.SCORE;
-                                    case NEUTRAL: return OppositeSide.Low.Cone.SCORE_TO_NEUTRAL;
-                                }
-                            case CUBE: 
-                                switch (currentMode) {
-                                    case INTAKE:
-                                    case READY: return OppositeSide.Low.Cube.READY;
-                                    case SCORE: return OppositeSide.Low.Cube.SCORE;
-                                    case NEUTRAL: return OppositeSide.Low.Cube.SCORE_TO_NEUTRAL;
-                                }
-                        }
+            case OPPOSITE:
+                switch (piece) {
+                    case CONE: return switchCone().switchSides();
+                    case CUBE: return switchCube().switchSides();
                 }
-            default:
-                return NEUTRAL;
-        }    
+            default: return NEUTRAL;
+        }        
     }
 
     public void setLevel(Level level) {
-        currentLevel = level;
+        this.level = level;
     }
 
     public void setPiece(Piece piece) {
-        currentPiece = piece;
+        this.piece = piece;
     }
 
     public void setSide(Side side) {
-        currentSide = side;
+        this.side = side;
     }
 
     public void setMode(Mode mode) {
-        currentMode = mode;
+        this.mode = mode;
     }
 }
