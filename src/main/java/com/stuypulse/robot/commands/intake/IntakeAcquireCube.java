2d9e742ebac102c2c8bb4d2358cb8acb776dab5e
Myles Pasetsky <mp3blue99@gmail.com>
2023-02-05 21:47:06 -0500

    Rename intake commands

--- src/main/java/com/stuypulse/robot/commands/intake/IntakeAcquireCube.java
+++ src/main/java/com/stuypulse/robot/commands/intake/IntakeAcquireCube.java
@@ -4,26 +4,16 @@ import com.stuypulse.robot.subsystems.intake.IIntake;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class OuttakeCone extends CommandBase{
+public class IntakeAcquireCube extends CommandBase{
     private IIntake intake;
-    public OuttakeCone(){
+
+    public IntakeAcquireCube(){
         intake = IIntake.getInstance();
         addRequirements(intake);
     }
 
     @Override
     public void initialize(){
-        intake.coneOuttake();
+        intake.acquireCube();
     }
-
-    @Override
-    public boolean isFinished(){
-        return false;
-    }
-
-    @Override
-    public void end(boolean interrupted){
-        intake.stop();
-    }
-    
 }
