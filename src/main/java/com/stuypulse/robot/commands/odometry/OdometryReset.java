6f5d9a179f51e16319e765feff63eec1e875aa36
Myles Pasetsky <mp3blue99@gmail.com>
2023-02-07 08:39:27 -0500

    Upgrade odometry reset

--- src/main/java/com/stuypulse/robot/commands/odometry/OdometryReset.java
+++ src/main/java/com/stuypulse/robot/commands/odometry/OdometryReset.java
@@ -1,5 +1,7 @@
 package com.stuypulse.robot.commands.odometry;
 
+import java.util.function.Supplier;
+
 import com.stuypulse.robot.subsystems.odometry.Odometry;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -8,10 +10,23 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class OdometryReset extends InstantCommand {
-    
-    public OdometryReset() {
-        super(() -> Odometry.getInstance().reset(new Pose2d(3.12,0.76, new Rotation2d(Units.degreesToRadians(0))))
-        );
+    private final Odometry odometry;
+    private final Supplier<Pose2d> references;
+
+    public OdometryReset(Supplier<Pose2d> references) {
+        odometry = Odometry.getInstance();
+        this.references = references;
+
+        // addRequirements(null);
+    }
+
+    public OdometryReset(Pose2d reference) {
+        this(() -> reference);
+    }
+
+    @Override
+    public void initialize() {
+        odometry.reset(references.get());
     }
 
 }
