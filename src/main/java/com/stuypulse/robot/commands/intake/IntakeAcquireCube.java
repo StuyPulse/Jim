767a0127e3f4af3f9a6d99ed4373339e7301a166
Oblivion409 <ben@goldfisher.com>
2023-02-10 17:48:47 -0500

    Make IntakeAcquireCube InstantCommand

--- src/main/java/com/stuypulse/robot/commands/intake/IntakeAcquireCube.java
+++ src/main/java/com/stuypulse/robot/commands/intake/IntakeAcquireCube.java
@@ -2,9 +2,9 @@ package com.stuypulse.robot.commands.intake;
 
 import com.stuypulse.robot.subsystems.intake.Intake;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
-public class IntakeAcquireCube extends CommandBase{
+public class IntakeAcquireCube extends InstantCommand {
     private Intake intake;
 
     public IntakeAcquireCube(){
