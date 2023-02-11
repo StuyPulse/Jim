5cdf41280831efd26ae116cb9d684d18df38fa46
Vincent Wang <vinowang921@gmail.com>
2023-02-11 00:40:22 -0500

    Update Auton Paths

--- src/main/java/com/stuypulse/robot/commands/auton/OnePieceDock.java
+++ src/main/java/com/stuypulse/robot/commands/auton/OnePieceDock.java
@@ -7,6 +7,7 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.stuypulse.robot.commands.intake.IntakeDeacquireCube;
 import com.stuypulse.robot.commands.arm.ArmFollowTrajectory;
+import com.stuypulse.robot.commands.swerve.SwerveDriveEngage;
 import com.stuypulse.robot.commands.swerve.SwerveDriveFollowTrajectory;
 import com.stuypulse.robot.constants.Settings.Swerve.Motion;
 
@@ -22,25 +23,26 @@ public class OnePieceDock extends SequentialCommandGroup {
     public OnePieceDock() {
         paths = SwerveDriveFollowTrajectory.getSeparatedPaths(
             PathPlanner.loadPathGroup("1 Piece + Dock", CONSTRAINTS, CONSTRAINTS),
-
-            "Mobility"
+            "1 Piece + Dock"
         );
 
         addCommands(
-            // new ArmFollowTrajectory(null),
-            new IntakeDeacquireCube(),
+            // new ArmFollowTrajectory(),
+            // new IntakeDeacquireCube(),
             new WaitCommand(INTAKE_DEACQUIRE_TIME),
             new SwerveDriveFollowTrajectory(
-                paths.get("Mobility")
-            ).robotRelative()
+                paths.get("1 Piece + Dock")
+            ).robotRelative(),
+
+            new SwerveDriveEngage()
         );
 
-        addCommands(
-            new SwerveDriveFollowTrajectory(
-                paths.get("Dock")
-            ).fieldRelative()
-            // new BasicGyroEngage(robot.swerve); 
-        );
+        // addCommands(
+        //     new SwerveDriveFollowTrajectory(
+        //         paths.get("Dock")
+        //     ).fieldRelative()
+        //     // new (robot.swerve); 
+        // );
     
     }
 
