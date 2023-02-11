7368febd83607976db639272e9dd34dbec1447b3
BenG49 <64862590+BenG49@users.noreply.github.com>
2023-02-11 18:05:25 -0500

    Fix PerfectArm

--- src/main/java/com/stuypulse/robot/subsystems/arm/PerfectArm.java
+++ src/main/java/com/stuypulse/robot/subsystems/arm/PerfectArm.java
@@ -1,36 +1,33 @@
 package com.stuypulse.robot.subsystems.arm;
 
-import com.stuypulse.robot.constants.Settings.Alignment.Rotation;
 import com.stuypulse.robot.util.ArmVisualizer;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class PerfectArm extends Arm {
 
+    private final ArmVisualizer visualizer = new ArmVisualizer(); 
+
     private Rotation2d shoulderAngle = Rotation2d.fromDegrees(90);
     private Rotation2d wristAngle = Rotation2d.fromDegrees(-90);
 
     @Override
     public Rotation2d getShoulderAngle() {
-        // TODO Auto-generated method stub
         return shoulderAngle;
     }
 
     @Override
     public Rotation2d getWristAngle() {
-        // TODO Auto-generated method stub
         return wristAngle;
     }
 
     @Override
     public Rotation2d getShoulderTargetAngle() {
-        // TODO Auto-generated method stub
-        return shoulderAngle    ;
+        return shoulderAngle;
     }
 
     @Override
     public Rotation2d getWristTargetAngle() {
-        // TODO Auto-generated method stub
         return wristAngle;
     }
 
@@ -42,27 +39,23 @@ public class PerfectArm extends Arm {
 
     @Override
     public void setTargetWristAngle(Rotation2d angle) {
-wristAngle = angle;
+        wristAngle = angle;
     }
 
     @Override
     public void setFeedbackEnabled(boolean enabled) {
         
-        
     }
 
-    // @Override
-    // public ArmVisualizer getVisualizer() {
-    //     // TODO Auto-generated method stub
-    //     return vis;
-    // }
-
-    ArmVisualizer vis = new ArmVisualizer(); 
+    @Override
+    public ArmVisualizer getVisualizer() {
+        return visualizer;
+    }
 
     @Override
     public void periodic() {
-        vis.setMeasuredAngles(shoulderAngle.getDegrees(), wristAngle.getDegrees());
-       vis.setTargetAngles(shoulderAngle.getDegrees(), wristAngle.getDegrees());
+        visualizer.setMeasuredAngles(shoulderAngle.getDegrees(), wristAngle.getDegrees());
+        visualizer.setTargetAngles(shoulderAngle.getDegrees(), wristAngle.getDegrees());
     }
     
 
