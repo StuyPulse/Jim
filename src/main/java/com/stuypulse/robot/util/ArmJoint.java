39e09b429f522dbfe25e6afe85bb8a2ba78caff4
Myles Pasetsky <mp3blue99@gmail.com>
2023-02-17 03:00:18 -0500

    Update arm joint

--- src/main/java/com/stuypulse/robot/util/ArmJoint.java
+++ src/main/java/com/stuypulse/robot/util/ArmJoint.java
@@ -1,12 +1,16 @@
 package com.stuypulse.robot.util;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+
 public class ArmJoint {
+    public final DCMotor motor;
     public final double mass;
     public final double length;
     public final double moi;
     public final double radius;
 
-    public ArmJoint(double mass, double length, double moi, double radius) {
+    public ArmJoint(DCMotor motor, double mass, double length, double moi, double radius) {
+        this.motor = motor;
         this.mass = mass;
         this.length = length;
         this.moi = moi;
