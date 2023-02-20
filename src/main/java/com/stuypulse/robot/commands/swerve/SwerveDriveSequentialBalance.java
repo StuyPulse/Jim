a0bb26e023c49b3794a5ba07370f22ebc8e2b71f
Shaurya Sen <shauryasen12@gmail.com>
2023-02-20 13:32:20 -0500

    Balance Alignment

--- src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveSequentialBalance.java
+++ src/main/java/com/stuypulse/robot/commands/swerve/SwerveDriveSequentialBalance.java
@@ -4,16 +4,30 @@ import com.stuypulse.robot.subsystems.odometry.Odometry;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class SwerveDriveSequentialBalance extends SequentialCommandGroup {
 
+    private double angle;
 
     public SwerveDriveSequentialBalance() {
+
+        double currentAngle = Odometry.getInstance().getRotation().getDegrees();
+        // angle
+        if (currentAngle > -45 && currentAngle <= 45) {
+            angle = 0;
+        } else if (currentAngle > 45 && currentAngle <= 135) {
+            angle = 90;
+        } else if (currentAngle < -45 && currentAngle >= -135) {
+            angle = -90;
+        } else if (currentAngle < -135 && currentAngle >= 135 ) {
+            angle = 180;
+        }
+
+
         addCommands(
-            new SwerveDriveToPose(() -> new Pose2d(Odometry.getInstance().getTranslation(), new Rotation2d())),
+            new SwerveDriveToPose(() -> new Pose2d(Odometry.getInstance().getTranslation(), new Rotation2d(angle))),
             new SwerveDriveBlayBalance(),
             new SwerveDrivePointWheels(Rotation2d.fromDegrees(90))
         );
