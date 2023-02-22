9018adb16acdd422a0e329f5e1dbf43ca4b6b198
Naowal Rahman <naowal.ar@gmail.com>
2023-02-22 15:09:17 -0500

    Add LED commands for autons

--- src/main/java/com/stuypulse/robot/commands/auton/OnePiecePickupDock.java
+++ src/main/java/com/stuypulse/robot/commands/auton/OnePiecePickupDock.java
@@ -4,11 +4,13 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.stuypulse.robot.commands.arm.routines.*;
 import com.stuypulse.robot.commands.intake.*;
+import com.stuypulse.robot.commands.leds.LEDSet;
 import com.stuypulse.robot.commands.manager.*;
 import com.stuypulse.robot.commands.plant.PlantEngage;
 import com.stuypulse.robot.commands.swerve.*;
 import com.stuypulse.robot.commands.swerve.balance.SwerveDriveBalanceWithPlant;
 import com.stuypulse.robot.subsystems.Manager.*;
+import com.stuypulse.robot.util.LEDColor;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
@@ -29,6 +31,7 @@ public class OnePiecePickupDock extends SequentialCommandGroup{
 
         // initial setup
         addCommands(
+            new LEDSet(LEDColor.RAINBOW),
             new ManagerSetNodeLevel(NodeLevel.HIGH),
             new ManagerSetGamePiece(GamePiece.CONE_TIP_IN),
             new ManagerSetIntakeSide(IntakeSide.FRONT),
@@ -37,11 +40,13 @@ public class OnePiecePickupDock extends SequentialCommandGroup{
 
         // score first piece
         addCommands(
+            new LEDSet(LEDColor.YELLOW.pulse()),
             new ArmReady(),
             new ArmScore(),
             new IntakeScore(),
             new WaitCommand(INTAKE_DEACQUIRE_TIME),
-            new IntakeStop()
+            new IntakeStop(),
+            new LEDSet(LEDColor.RAINBOW)
         );
 
         // intake second piece
@@ -49,26 +54,32 @@ public class OnePiecePickupDock extends SequentialCommandGroup{
             new ManagerSetNodeLevel(NodeLevel.MID),
             new ManagerSetGamePiece(GamePiece.CUBE),
 
+            new LEDSet(LEDColor.GREEN),
             new SwerveDriveFollowTrajectory(
                 paths.get("Intake Piece"))
                     .robotRelative()
                     .addEvent("ReadyIntakeOne", new ArmIntake().andThen(new IntakeAcquire()))
                     .withEvents(),
 
+            new LEDSet(LEDColor.YELLOW),
             new IntakeWaitForPiece().withTimeout(INTAKE_ACQUIRE_TIME),
-            new IntakeStop()
+            new IntakeStop(),
+            new LEDSet(LEDColor.RAINBOW)
         );
         
         // dock and engage
         addCommands(
+            new LEDSet(LEDColor.GREEN),
             new SwerveDriveFollowTrajectory(
                 paths.get("Dock"))
                     .fieldRelative()
                     .addEvent("ArmNeutral", new ArmNeutral())
                     .withEvents(),
                     
+            new LEDSet(LEDColor.RED.pulse()),
             new SwerveDriveBalanceWithPlant().withTimeout(ENGAGE_TIME),
-            new PlantEngage()
+            new PlantEngage(),
+            new LEDSet(LEDColor.BLUE)
         );
     
     }
