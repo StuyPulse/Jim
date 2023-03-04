9e0a6fc7280561d6e969eeba4b26da7e1c27c2a6
Myles Pasetsky <mp3blue99@gmail.com>
2023-03-03 20:27:33 -0500

    Add open loop ramp rate to intake

--- src/main/java/com/stuypulse/robot/constants/Motors.java
+++ src/main/java/com/stuypulse/robot/constants/Motors.java
@@ -30,8 +30,8 @@ public interface Motors {
     }
 
     public interface Intake {
-        CANSparkMaxConfig FRONT_MOTOR = new CANSparkMaxConfig(false, IdleMode.kBrake, 40);
-        CANSparkMaxConfig BACK_MOTOR = new CANSparkMaxConfig(true, IdleMode.kBrake, 40);
+        CANSparkMaxConfig FRONT_MOTOR = new CANSparkMaxConfig(false, IdleMode.kBrake, 40, .1);
+        CANSparkMaxConfig BACK_MOTOR = new CANSparkMaxConfig(true, IdleMode.kBrake, 40, .1);
     }
 
     public interface Swerve {
