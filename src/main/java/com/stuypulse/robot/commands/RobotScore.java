17a452b5670cc08d42ff0b0a023ecb0135b1344a
Prog694 <prog692@gmail.com>
2023-03-06 11:53:18 -0500

    Change end state of RobotScore for tip in

--- src/main/java/com/stuypulse/robot/commands/RobotScore.java
+++ src/main/java/com/stuypulse/robot/commands/RobotScore.java
@@ -71,7 +71,11 @@ public class RobotScore extends CommandBase {
     @Override
     public void end(boolean i) {
         // holds arm in place
-        arm.setTargetState(arm.getState());
+        // arm.setTargetState(arm.getState());
+        
+        if (Manager.getInstance().getGamePiece() == GamePiece.CONE_TIP_IN) { 
+            arm.setWristVoltage(0);
+        }
 
         swerve.stop();
     }
