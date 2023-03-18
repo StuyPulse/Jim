b36d66c9ec7f823141e86d329f39d84c4517006a
Vincent Wang <vinowang921@gmail.com>
2023-03-18 14:46:31 -0400

    Fix spacing

--- src/main/java/com/stuypulse/robot/constants/ArmTrajectories.java
+++ src/main/java/com/stuypulse/robot/constants/ArmTrajectories.java
@@ -9,8 +9,8 @@ public interface ArmTrajectories {
 
     public interface Acquire {
         ArmState kCone = new ArmState(
-			      new SmartNumber("Arm Trajectories/Acquire Cone Tip In Front Shoulder", -70.82),
-			      new SmartNumber("Arm Trajectories/Acquire Cone Tip In Front Wrist", 7.3));
+            new SmartNumber("Arm Trajectories/Acquire Cone Tip In Front Shoulder", -70.82),
+            new SmartNumber("Arm Trajectories/Acquire Cone Tip In Front Wrist", 7.3));
         ArmState kCube = new ArmState(
             new SmartNumber("Arm Trajectories/Acquire Cube Front Shoulder", -70.82),
             new SmartNumber("Arm Trajectories/Acquire Cube Front Wrist", 8.37));
