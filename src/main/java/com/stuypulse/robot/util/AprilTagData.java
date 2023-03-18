09f71859986cbf4560e8c328ced7d2c73db808af
vincentw <vinowang921@gmail.com>
2023-03-18 14:47:37 -0400

    Robot March 17 (#133)
    * Add default limelight pose3ds
    
    * Reorganize limelight code
    
    * Put in a valid limelight campose
    
    * Move edge case checking into Vision putAprilTagData
    
    * Add opposite side april tag positions (#128)
    
    * Change button bindings, remove robot relative drive and slow drive, back up in robot release for tip out
    
    * Put arm tuning changes into March 17 (#134)
    
    * Robot changes from March 14th (#131)
    
    * Fix zero offset on front left
    
    * Fix arm flaccid after teleop
    
    * Add tip out high scoring
    
    ---------
    
    Co-authored-by: Vincent Wang <vinowang921@gmail.com>
    
    * BOOM Retune Arm and Swerve
    
    * Tune intake and score values
    
    * Remove set arm target in robot release
    
    ---------
    
    Co-authored-by: Myles Pasetsky <34213819+selym3@users.noreply.github.com>
    
    * Remove robotrelative
    
    * Clean driver bindings
    
    * BOOM Fix limelight poses and methods (#132)
    
    Co-authored-by: Vincent Wang <vinowang921@gmail.com>
    
    * Remove decimals from arm trajectory
    
    * Revamp swerve filters and add thrust button (#130)
    
    * Add Thrust
    
    Co-authored-by: Richie Xue <rxue40@stuy.edu>
    
    * Fix code
    
    * GYRO
    
    Co-authored-by: Zixi52 <Zixi52@users.noreply.github.com>
    Co-authored-by: Baiulus <Baiulus@users.noreply.github.com>
    
    * Fix gyro feedback anguar velocity
    
    Co-authored-by: chubclaire <chubclaire@users.noreply.github.com>
    Co-authored-by: MdyWg <MdyWg@users.noreply.github.com>
    Co-authored-by: amyk07 <amyk07@users.noreply.github.com>
    
    * Fix interpolate
    
    * Change bindings, use gyro angle, don't gyro feedback if stopped, add constants
    
    * Bindings changes
    
    * Make hold angle empty at swerve drive drive init
    
    * Fix score pose constants
    
    * Update Alignment Pose Field Translations
    
    * Remove thrust
    
    ---------
    
    Co-authored-by: Richie Xue <rxue40@stuy.edu>
    Co-authored-by: Zixi52 <Zixi52@users.noreply.github.com>
    Co-authored-by: Baiulus <Baiulus@users.noreply.github.com>
    Co-authored-by: Naowal Rahman <naowal.ar@gmail.com>
    Co-authored-by: chubclaire <chubclaire@users.noreply.github.com>
    Co-authored-by: MdyWg <MdyWg@users.noreply.github.com>
    Co-authored-by: amyk07 <amyk07@users.noreply.github.com>
    Co-authored-by: Vincent Wang <vinowang921@gmail.com>
    
    * Fix RobotRelease smartnumber naming
    
    * Remove hold arm in place from score and release
    
    * Remove debug smartnumber
    
    * Remove unused alignment profile constants
    
    * Remove duplicate constant
    
    * Replace Limelight in ApriltagData with Pose3d
    
    * Fix spacing
    
    ---------
    
    Co-authored-by: Prog694 <prog692@gmail.com>
    Co-authored-by: Myles Pasetsky <34213819+selym3@users.noreply.github.com>
    Co-authored-by: BenG49 <64862590+BenG49@users.noreply.github.com>
    Co-authored-by: ShauryaSen <66851598+ShauryaSen@users.noreply.github.com>
    Co-authored-by: Richie Xue <rxue40@stuy.edu>
    Co-authored-by: Zixi52 <Zixi52@users.noreply.github.com>
    Co-authored-by: Baiulus <Baiulus@users.noreply.github.com>
    Co-authored-by: Naowal Rahman <naowal.ar@gmail.com>
    Co-authored-by: chubclaire <chubclaire@users.noreply.github.com>
    Co-authored-by: MdyWg <MdyWg@users.noreply.github.com>
    Co-authored-by: amyk07 <amyk07@users.noreply.github.com>
    Former-commit-id: e476d59702d414061b3bde4c984348ad93eb339e

--- src/main/java/com/stuypulse/robot/util/AprilTagData.java
+++ src/main/java/com/stuypulse/robot/util/AprilTagData.java
@@ -1,22 +1,50 @@
 package com.stuypulse.robot.util;
 
+import com.stuypulse.robot.constants.Field;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class AprilTagData {
     public final Pose2d pose;
     public final double latency;
     public final int id;
+    public final Pose3d cameraPose;
+
+    private static double getDegreesBetween(Rotation2d a, Rotation2d b) {
+        double c = a.getCos() * b.getCos() + a.getSin() * b.getSin();
+        double d = (1 - c) * 180;
+
+        return d;
+    }
+
+    public double getDegreesToTag() {
+        Rotation2d tag = Field.getAprilTagFromId(id).getRotation();
+
+        return getDegreesBetween(tag.plus(Rotation2d.fromDegrees(180)), pose.getRotation().minus(cameraPose.getRotation().toRotation2d()));
+    }
+
+    public double getDistanceToTag() {
+        Translation2d robot = pose.getTranslation();
+        Translation2d tag = Field.getAprilTagFromId(id).getTranslation();
+        
+        return robot.getDistance(tag);
+    }
 
     /**
      * Record april tag data, usually from a vision system
      * 
-     * @param pose robot pose information, in meters with blue/red corner as origin
+     * @param robotPose robot pose information, in meters with blue/red corner as origin
      * @param latency total latency, in seconds
      * @param id tag id
+     * @param cameraPose associated camera's robot relative pose
      */
-    public AprilTagData(Pose2d pose, double latency, int id) {
-        this.pose = pose;
+    public AprilTagData(Pose2d robotPose, double latency, int id, Pose3d cameraPose) {
+        this.pose = robotPose;
         this.latency = latency;
         this.id = id;
+        this.cameraPose = cameraPose;
     }
 }
