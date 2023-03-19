735e29d8a1dfe9a2c57896d21436f7740553e319
Myles Pasetsky <mp3blue99@gmail.com>
2023-03-19 18:07:13 -0400

    rremove silly sim voltage stuff

--- src/main/java/com/stuypulse/robot/subsystems/arm/SimArm.java
+++ src/main/java/com/stuypulse/robot/subsystems/arm/SimArm.java
@@ -6,7 +6,6 @@ import static com.stuypulse.robot.constants.Settings.Arm.*;
 
 import com.stuypulse.robot.util.ArmDynamics;
 import com.stuypulse.robot.util.TwoJointArmSimulation;
-import com.stuypulse.stuylib.network.SmartNumber;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 
@@ -15,15 +14,15 @@ public class SimArm extends Arm {
     private final ArmDynamics dynamics;
     private final TwoJointArmSimulation simulation;
 
-    private SmartNumber shoulderVolts = new SmartNumber("Arm/Shoulder/Sim Voltage (Test)", 0);
-    private SmartNumber wristVolts = new SmartNumber("Arm/Wrist/Sim Voltage (Test)", 0);
+    private double shoulderVolts;
+    private double wristVolts;
 
     protected SimArm() { 
         dynamics = new ArmDynamics(Shoulder.JOINT, Wrist.JOINT);
         simulation = new TwoJointArmSimulation(-Math.PI/2, Math.PI/2, dynamics);
         
-        // shoulderVolts = 0;
-        // wristVolts = 0;
+        shoulderVolts = 0;
+        wristVolts = 0;
     }
 
     @Override
@@ -38,17 +37,17 @@ public class SimArm extends Arm {
 
 	@Override
 	protected void setShoulderVoltageImpl(double voltage) {
-		shoulderVolts.set(voltage);
+		shoulderVolts = voltage;
 	}
 
 	@Override
     protected void setWristVoltageImpl(double voltage) {
-		wristVolts.set(voltage);
+		wristVolts = voltage;
 	}
 
     @Override
     public void periodicallyCalled() {
-        simulation.update(shoulderVolts.get(), wristVolts.get(), Settings.DT);
+        simulation.update(shoulderVolts, wristVolts, Settings.DT);
     }
 
     @Override
