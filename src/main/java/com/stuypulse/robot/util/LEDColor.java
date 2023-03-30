4cc60428012986913c9e3eae63062d06268c8016
reyamiller <72947255+reyamiller@users.noreply.github.com>
2023-03-30 10:07:21 -0400

    switch to using AddressableLED and RGB values (#148)
    * switch to using AddressableLED and RGB values
    
    * update leds
    
    * fixed leds
    
    * remove simgui-window
    
    * Remove unused method
    
    ---------
    
    Co-authored-by: colyi <colyichen82@gmail.com>
    Co-authored-by: Myles Pasetsky <mp3blue99@gmail.com>
    Co-authored-by: Myles Pasetsky <34213819+selym3@users.noreply.github.com>
    Former-commit-id: ce68ff6caf6986011347e6eac110e8fc9d560839

--- src/main/java/com/stuypulse/robot/util/LEDColor.java
+++ src/main/java/com/stuypulse/robot/util/LEDColor.java
@@ -8,83 +8,73 @@ package com.stuypulse.robot.util;
 import com.stuypulse.robot.constants.Settings;
 
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.util.Color8Bit;
 
 /**
- * Class that stores all of the different PWM values for the LED Controller.
+ * Class that stores all of the different RGB values for the LED Controller.
  *
  * @author Sam Belliveau
  * @author Andrew Liu
+ * @author Reya Miller
+ * @author Colyi Chen
  */
 public class LEDColor {
-    private final double color;
-    private final boolean pulse;
+    private final int red;
+    private final int green;
+    private final int blue; 
 
-    private LEDColor(double color, boolean pulse) {
-        this.color = color;
-        this.pulse = pulse;
+    private LEDColor(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
     }
 
-    private LEDColor(double color) {
-        this(color, false);
+    public int getRed() {
+        return red;
     }
 
-    public LEDColor pulse() {
-        return new LEDColor(this.color, true);
+    public int getGreen() {
+        return green;
     }
-
-    public double get() {
-        // Variables for detecting if we should be blinking or not
-        double cT = Timer.getFPGATimestamp() % (Settings.LED.BLINK_TIME);
-        double oT = (0.5 * Settings.LED.BLINK_TIME);
-
-        // Detect if the color should be on or off
-        if (pulse && cT >= oT) {
-            return LEDColor.OFF.color;
-        } else {
-            return this.color;
-        }
+    
+    public int getBlue() {
+        return blue;
+    }
+    
+    public LEDColor setColor(int red, int green, int blue) {
+        return new LEDColor(red, green, blue);
     }
 
     /***********************/
     /*** COLOR CONSTANTS ***/
     /***********************/
 
-    public static final LEDColor BEAT = new LEDColor(-0.67);
 
-    public static final LEDColor BREATH = new LEDColor(-0.17);
-    public static final LEDColor CONFETTI = new LEDColor(-0.87);
-    public static final LEDColor FIRE = new LEDColor(0.57);
-    public static final LEDColor HEARTBEAT = new LEDColor(-0.25);
-    public static final LEDColor SINELON = new LEDColor(-0.77);
-    public static final LEDColor TWINKLES = new LEDColor(-0.53);
-    public static final LEDColor WAVE = new LEDColor(-0.43);
-    public static final LEDColor RAINBOW = new LEDColor(-0.99);
+    public static final LEDColor AQUA = new LEDColor(0, 255, 255);
+    public static final LEDColor BLACK = new LEDColor(0, 0, 0);
+    public static final LEDColor BLUE = new LEDColor(0, 128, 255);
+    public static final LEDColor BLUE_GREEN = new LEDColor(0, 255, 128);
+    public static final LEDColor BLUE_VIOLET = new LEDColor(51, 51, 255);
+    public static final LEDColor DARK_BLUE = new LEDColor(0, 0, 204);
+    public static final LEDColor DARK_GRAY = new LEDColor(64, 64, 64);
+    public static final LEDColor DARK_GREEN = new LEDColor(0, 153, 0);
+    public static final LEDColor DARK_RED = new LEDColor(204, 0, 0);
+    public static final LEDColor GOLD = new LEDColor(218,165,32);
+    public static final LEDColor GRAY = new LEDColor(128, 128, 128);
+    public static final LEDColor GREEN = new LEDColor(0, 255, 0);
+    public static final LEDColor HOT_PINK = new LEDColor(255, 105, 180);
+    public static final LEDColor LAWN_GREEN = new LEDColor(102, 204, 0);
+    public static final LEDColor LIME = new LEDColor(191, 255, 0);
+    public static final LEDColor ORANGE = new LEDColor(255, 128, 0);
+    public static final LEDColor PINK = new LEDColor(255, 192, 203);
+    public static final LEDColor PURPLE = new LEDColor(160, 32, 240);
+    public static final LEDColor RED = new LEDColor(255, 0 , 0);
+    public static final LEDColor RED_ORANGE = new LEDColor(255, 83, 73);
+    public static final LEDColor VIOLET = new LEDColor(127, 0, 255);
+    public static final LEDColor WHITE = new LEDColor(255, 255, 255);
+    public static final LEDColor YELLOW = new LEDColor(255, 255, 0);
 
-    public static final LEDColor AQUA = new LEDColor(0.81);
-    public static final LEDColor BLACK = new LEDColor(0.99);
-    public static final LEDColor BLUE = new LEDColor(0.87);
-    public static final LEDColor BLUE_GREEN = new LEDColor(0.79);
-    public static final LEDColor BLUE_VIOLET = new LEDColor(0.89);
-    public static final LEDColor DARK_BLUE = new LEDColor(0.85);
-    public static final LEDColor DARK_GRAY = new LEDColor(0.97);
-    public static final LEDColor DARK_GREEN = new LEDColor(0.75);
-    public static final LEDColor DARK_RED = new LEDColor(0.59);
-    public static final LEDColor GOLD = new LEDColor(0.67);
-    public static final LEDColor GRAY = new LEDColor(0.95);
-    public static final LEDColor GREEN = new LEDColor(0.77);
-    public static final LEDColor HOT_PINK = new LEDColor(0.57);
-    public static final LEDColor LAWN_GREEN = new LEDColor(0.71);
-    public static final LEDColor LIME = new LEDColor(0.73);
-    public static final LEDColor ORANGE = new LEDColor(0.65);
-    public static final LEDColor PINK = new LEDColor(0.57);
-    public static final LEDColor PURPLE = new LEDColor(0.91);
-    public static final LEDColor RED = new LEDColor(0.61);
-    public static final LEDColor RED_ORANGE = new LEDColor(0.63);
-    public static final LEDColor SKY_BLUE = new LEDColor(0.83);
-    public static final LEDColor VIOLET = new LEDColor(0.91);
-    public static final LEDColor WHITE = new LEDColor(0.93);
-    public static final LEDColor YELLOW = new LEDColor(0.69);
+    public static final LEDColor OFF = new LEDColor(0, 0, 0);
+    public static final LEDColor RAINBOW = OFF;
 
-    public static final LEDColor OFF = new LEDColor(0.99);
+    
 }
