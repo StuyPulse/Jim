6f1392b028acdfd909cfbf77c03ec0171a7d0e72
Naowal Rahman <naowal.ar@gmail.com>
2023-04-05 17:47:31 -0400

    Cleaned up LED + auton code, made Richie mode
    Co-authored-by: colyic <colyic@users.noreply.github.com>
    Co-authored-by: Souvik Basak <awildhooman@users.noreply.github.com>
    Co-authored-by: Ian Shi <IanShiii@users.noreply.github.com>
    Co-authored-by: Kalimul <Kalimul@users.noreply.github.com>
    Co-authored-by: Mustafa <bananaeater321@users.noreply.github.com>

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -1,4 +1,4 @@
-/************************ PROJECT PHIL ************************/
+    /************************ PROJECT PHIL ************************/
 /* Copyright (c) 2023 StuyPulse Robotics. All rights reserved.*/
 /* This work is licensed under the terms of the MIT license.  */
 /**************************************************************/
@@ -153,7 +153,7 @@ public class RobotContainer {
         // plant
         //driver.getRightButton().onTrue(new PlantEngage());
 
-        driver.getRightButton().whileTrue(new LEDSetRainbow());
+        driver.getRightButton().whileTrue(new LEDSet(LEDColor.RAINBOW));
         driver.getRightBumper().onTrue(new PlantDisengage());
 
         new Trigger(intake::hasCone)
