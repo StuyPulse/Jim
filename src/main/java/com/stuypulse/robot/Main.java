552a512ae642157b8e87128380d591962db37d42
Prog694 <prog692@gmail.com>
2023-04-06 08:55:13 -0400

    remove test project from main

--- src/main/java/com/stuypulse/robot/Main.java
+++ src/main/java/com/stuypulse/robot/Main.java
@@ -7,9 +7,6 @@ package com.stuypulse.robot;
 
 import com.stuypulse.stuylib.util.StopWatch;
 
-import com.stuypulse.robot.constants.Settings;
-import com.stuypulse.robot.test.Testbot;
-
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
@@ -18,10 +15,6 @@ public final class Main {
 
     public static void main(String... args) {
         StopWatch.setDefaultEngine(StopWatch.kFPGAEngine);
-        if (Settings.ROBOT == Settings.Robot.BLAY_MODE) {
-            RobotBase.startRobot(Testbot::new);
-        } else {
-            RobotBase.startRobot(Robot::new);
-        }
+        RobotBase.startRobot(Robot::new);
     }
 }
